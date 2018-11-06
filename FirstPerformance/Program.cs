d2aa84dfcd53f7fcf5cefb5d7bf16c879dcc714c
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 07:25:39 -0300

    My First Program made in C#

---
+++ FirstPerformance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FirstPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, Delta, x1, x2;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0]);
+            b = double.Parse(vet[1]);
+            c = double.Parse(vet[2]);
+
+            Delta = b * b - 4 * a * c ;
+
+            if (a == 0.0 || Delta <= 0.0)
+            {
+                Console.WriteLine("IMPOSSIVEL CALCULAR");
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(Delta)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(Delta)) / (2.0 * a);
+
+                Console.WriteLine("X1 = " + x1);
+                Console.WriteLine("X2 = " + x2);
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
